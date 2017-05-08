a4cf22d3e67c1344433872dd9d4c57f3a89b3404
negative <negative.officiall@gmail.com>
2017-05-08 14:09:26 +0430

    Private Message

--- app/Utils/OnMessage.php
+++ app/Utils/OnMessage.php
@@ -9,8 +9,16 @@ class OnMessage
 
     public function __construct($updates)
     {
+        $bot = new Bot();
+
         if($updates['chat']['type'] != "private"){
             Commands::check($updates);
+        }else{
+            $bot->apiRequest("sendMessage",[
+                "chat_id"=>$updates['chat']['id'],
+                "text"=>"<b>Admin</b> : @negative\n<b>Support Group</b> : https://t.me/joinchat/AAAAAESEC_cBgp1VtYErHA",
+                "parse_mode"=>"html"
+            ]);
         }
     }
 }
